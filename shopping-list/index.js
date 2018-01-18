dd373c298c0be0109021b9de18dd842adc680549
Chris Yang <cyang287@yahoo.com>
2018-01-18 16:40:53 -0500

    finished shopping-list

---
+++ shopping-list/index.js
@@ -0,0 +1,30 @@
+'use strict';
+
+$(function() {
+  $('#js-shopping-list-form').submit(function(event) {
+    event.preventDefault();
+    const userText = $(event.currentTarget).find('.js-shopping-list-entry');
+
+    $('.shopping-list').append(`
+      <li>
+        <span class="shopping-item">${userText.val()}</span>
+        <div class="shopping-item-controls">
+          <button class="shopping-item-toggle">
+            <span class="button-label">check</span>
+          </button>
+          <button class="shopping-item-delete">
+            <span class="button-label">delete</span>
+          </button>
+        </div>
+      </li>
+    `);
+  });
+
+  $('.shopping-list').on('click', '.shopping-item-toggle', function(event) {
+    $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
+  });
+
+  $('.shopping-list').on('click', '.shopping-item-delete', function(event) {
+    this.closest('li').remove();
+  });
+});
